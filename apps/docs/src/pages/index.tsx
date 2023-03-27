3077c58427d3078e63ebfab34ede370f3020f83b
AndyOoh <andy.oeee@gmail.com>
2023-03-28 05:34:15 +0700

    komo

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -10,9 +10,9 @@ export default function Home() {
 
       <main className='mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8'>
         <h1 className='mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl'>
-          Landing
+          Landing is here
           <span className='block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2'>
-            testing lalala
+            testing 
 
           </span>
         </h1>
