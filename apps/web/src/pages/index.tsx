8ab5115dcf59f2490616009354ca6d852ae86930
AndyOoh <andy.oeee@gmail.com>
2023-03-28 03:50:02 +0700

    test again

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
         <h1 className='mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl'>
           Dev (app)
           <span className='block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2'>
-            Turborepo Example - testing deploy on release lalalala
+            Create a yaml for each vercel project. They can be simple: on release curl hook app.my-doamin. on push curl hook landing.my-domain. and keep teh ignore buiold steps to filter changes.
           </span>
         </h1>
         <div className='mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8'>
