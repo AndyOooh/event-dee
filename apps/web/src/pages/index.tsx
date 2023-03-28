f1e022f21e2070164cb3736836e9c6fed2ebd0c5
AndyOoh <andy.oeee@gmail.com>
2023-03-29 02:59:52 +0700

    pp

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -30,8 +30,7 @@ export default function Home() {
         <h1 className='mx-auto text-center text-6xl font-extrabold tracking-tight text-white sm:text-7xl lg:text-8xl xl:text-8xl'>
           Dev (app)
           <span className='block bg-gradient-to-r from-brandred to-brandblue bg-clip-text text-transparent px-2'>
-            Create a yaml for each vercel project. They can be simple: on release curl hook app.my-doamin. on push curl hook landing.my-domain. and keep teh ignore buiold steps to filter changes.
-            That is good ØØØ
+            Hey!
           </span>
         </h1>
         <div className='mx-auto mt-5 max-w-xl sm:flex sm:justify-center md:mt-8'>
@@ -44,7 +43,6 @@ export default function Home() {
           rel='noreferrer'>
           Go to landing now! Should not e to qq 22qqqq
         </a>
-
         <div className='mt-12 grid grid-cols-1 sm:grid-cols-3 gap-4 place-content-evenly'>
           {CARD_CONTENT.map(card => (
             <Card key={card.title} {...card} />
